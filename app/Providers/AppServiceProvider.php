44215499f5f563de5cc992eb5e79114fc3dba628
Prince Antwi <acrisprince@gmail.com>
2023-06-18 08:31:09 +0000

    my commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,25 +4,20 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+namespace App\Providers;
+
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\ServiceProvider;
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+    // ...
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
+
